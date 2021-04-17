338fecc2acaaacc3e309e6dd209c919e754d241d
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2021-04-16 20:15:23 -0700

    add 5.3

---
+++ eopi/5.primitive_types/5.3.cpp
@@ -0,0 +1,9 @@
+// take in 64 bit number, reverse bits and spit out new number
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // do same as 5.1, precompute reversed for every i, 16-bits...and then do an OR for each (4 of) 64 bits and return res
+}
