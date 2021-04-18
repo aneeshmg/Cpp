20f905476284be41004c5ab10ad573d9ea2461ad
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2021-04-17 19:22:52 -0700

    add 5.10

---
+++ eopi/5.primitive_types/5.10.cpp
@@ -0,0 +1,14 @@
+// get gcd without using division, multiplication or the mod operator
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+
+}
+
+int main() {
+
+    int a = 15, b = 20;
+    // understood the logic, implement during rev
+}
