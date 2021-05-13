ab19acf4c20088f5dad1c0ef17c603f446c47e95
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2021-05-12 20:35:27 -0700

    add 11.5

---
+++ eopi/11.search/11.5.cpp
@@ -0,0 +1,24 @@
+// search in sorted-rotated array for smallest
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+int search_smallest(const vector<T> &A) {
+    int l = 0, r = A.size() - 1;
+    while (l <= r) {
+        int m = l + ((r - l) >> 1);
+        if (A[m] > A[r])
+            l = m + 1;
+        else
+            r = m;
+    }
+    return l;
+}
+
+int main() {
+
+    // fairly simple, clever
+
+}
