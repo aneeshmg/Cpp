438e37d1cafac1e04c815d0c349a7e6ae025eb70
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2021-05-12 20:46:10 -0700

    add 11.6

--- eopi/11.search/11.5.cpp
+++ eopi/11.search/11.5.cpp
@@ -1,24 +1,53 @@
-// search in sorted-rotated array for smallest
+// search in sorted-rotated array of unknown length
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 template <typename T>
-int search_smallest(const vector<T> &A) {
-    int l = 0, r = A.size() - 1;
+int binary_search_unknown_length(const vector<T> &A, const T &k) {
+    // find the possible range where k exists
+    int p = 0;
+
+    while (true) {
+        try {
+            T val = A.at((1 << p) - 1);
+            if (val == k) {
+                return (1 << p) - 1;
+            } else if (val > k) {
+                break;
+            }
+        } catch (exception &e) {
+            break;
+        }
+        ++p;
+    }
+
+    // binary search between 2^(p - 1) + 1 to 2^p - 2
+    int l = (1 << (p - 1)) + 1, r = (1 << p) - 2;
+
     while (l <= r) {
         int m = l + ((r - l) >> 1);
-        if (A[m] > A[r])
-            l = m + 1;
-        else
-            r = m;
+
+        try {
+            T val = A.at(m);
+            if (val == k) {
+                return m;
+            } else if (val > k) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        } catch (exception &e) {
+            r = m - 1;
+        }
     }
-    return l;
+
+    return -1;
 }
 
 int main() {
 
-    // fairly simple, clever
+    // relatively complex, but understandable
 
 }
---
+++ eopi/11.search/11.6.cpp
@@ -0,0 +1,15 @@
+// 2-sum in an abs-sorted array
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// template <typename T>
+
+
+int main() {
+
+    // simple enough...simpler solution is in 12th chap
+    // 2sum....sorted or abs-sorted or whatever, can be found in 2sum using hashmap
+
+}
