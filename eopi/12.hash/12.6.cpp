205c7875bfd968b1a6e687def3798b71b0fbad06
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2021-05-25 12:20:19 -0700

    add 12.6

---
+++ eopi/12.hash/12.6.cpp
@@ -0,0 +1,15 @@
+// find similar users
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <numeric>
+
+using namespace std;
+
+// compute hash function over the set of attributes and using the hash determine the similarity
+// no code provided
+
+int main() {
+
+    // fairly straight forward
+}
