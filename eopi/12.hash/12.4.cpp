4a75ba1b2bc2120843ead2d59c357eae9a39b83b
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2021-05-25 12:04:13 -0700

    add 12.4

---
+++ eopi/12.hash/12.4.cpp
@@ -0,0 +1,16 @@
+// compression of multiple BSTs
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <numeric>
+
+using namespace std;
+
+// no cpp code present
+// flyweight pattern
+// canonicalization of BST
+
+int main() {
+
+    // 
+}
