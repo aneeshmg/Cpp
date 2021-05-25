3f30380638eb9b26b9ad62f6ee68a0ffe8856662
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2021-05-25 12:13:10 -0700

    add 12.5

---
+++ eopi/12.hash/12.5.cpp
@@ -0,0 +1,15 @@
+// pair users by set of attributes
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <numeric>
+
+using namespace std;
+
+// no code given but straightforward
+// parse attributes and use attributes as keys and users as values
+
+int main() {
+
+    // fairly straight forward
+}
